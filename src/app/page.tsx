3ab1898c57b9a4ce09f693e42758e01148ae63d0
AyanAlikhan11 <scoutayan853@gmail.com>
2025-12-23 23:40:35 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 
 import Footer from "@/components/Footer";
-import Herosection from "@/components/Herosection";
 import Navbar from "@/components/Navbar";
+import EmergencyCTA from "@/components/EmergencyCTA";
+import Testimonials from "@/components/Testimonials";
+import FloatingBloodCells from "@/components/FloatingBloodCells";
+import HeroSection from "@/components/Herosection";
+import StatsSection from "@/components/StatsSection";
+import HowItWorks from "@/components/HowItWorks";
+import PartnerHospitals from "@/components/partnershospital";
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased
-    bg-grid-white/[0.02]">
-      <Navbar />
-    
-      <Herosection />
-      <Footer />
-    </main>
+    <div className="relative min-h-screen overflow-hidden">
+    <FloatingBloodCells/>
+<Navbar />
+<HeroSection />
+<StatsSection />
+<HowItWorks /> 
+<PartnerHospitals/>
+<Testimonials />
+<EmergencyCTA />
+<Footer />
+</div>
+
   );
 }
